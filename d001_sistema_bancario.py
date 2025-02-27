db57be8863750b8acd815ea301350fec39f0ca97
DaiDVieira <daianevieira.ufsm@gmail.com>
2025-02-27 19:49:28 -0300

    saque

--- d001_sistema_bancario.py
+++ d001_sistema_bancario.py
@@ -1,19 +1,37 @@
 def depositar(saldo_atual, valor, extrato, /):
     if valor > 0:
         saldo_atual += valor
-        extrato += f"Depósito:\t R$ {valor: .2f}\n"
+        extrato += f"Depósito:\t R$ {valor:.2f}\n"
     else:
         print("Valor de depósito inválido. Tente novamente.")
+
     return saldo_atual, extrato
 
 def exibir_extrato(saldo_atual, extrato, /):
     print("------------- Extrato ------------")
     if not extrato:
-        print("Não foram realizadas movimentações")    
+        print("Não foram realizadas movimentações.")    
     else:   
         print(extrato)
     print(f"\nSaldo:\t\t R$ {saldo_atual:.2f}")
     print("----------------------------------")
+
+def sacar(saldo_atual, valor, extrato, numero_saques, limite_valor, /):
+    if valor > 0:
+        if saldo_atual < valor:
+            print("Saldo insuficiente. Não será possível realizar o saque.")
+        else:
+            if valor >= limite_valor:
+                print(f"Não é possível sacar valores maiores que R$ {limite_valor}. Tente novamente.")
+            else:
+                saldo_atual -= valor
+                numero_saques += 1
+                extrato += f"Saque:\t\t R$ {valor:.2f}\n"
+
+    else:
+        print("Valor de saque inválido. Tente novamente.")
+
+    return saldo_atual, extrato, numero_saques
 
 def main():
     menu = """
@@ -39,7 +57,11 @@ def main():
             saldo, extrato = depositar(saldo, valor, extrato)
             
         elif opcao == "s":
-            print("Saque")
+            if numero_saques == LIMITE_SAQUES:
+                print(f"Número máximo de {LIMITE_SAQUES} saques diários atingido. Tente novamente amanhã.")
+            else:
+                valor = float(input("\nDigite o valor do saque: "))
+                saldo, extrato, numero_saques = sacar(saldo, valor, extrato, numero_saques, limite)
 
         elif opcao == "e":
             exibir_extrato(saldo, extrato)
