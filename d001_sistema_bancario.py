9d1727bf61b1cffd3669dc8e9d5741298d2ee63a
DaiDVieira <daianevieira.ufsm@gmail.com>
2025-02-27 19:20:14 -0300

    funcoes de deposito e extrato

--- d001_sistema_bancario.py
+++ d001_sistema_bancario.py
@@ -1,4 +1,23 @@
-menu = """
+def depositar(saldo_atual, valor, extrato, /):
+    if valor > 0:
+        saldo_atual += valor
+        extrato += f"Depósito:\t R$ {valor: .2f}\n"
+    else:
+        print("Valor de depósito inválido. Tente novamente.")
+    return saldo_atual, extrato
+
+def exibir_extrato(saldo_atual, extrato, /):
+    print("------------- Extrato ------------")
+    if not extrato:
+        print("Não foram realizadas movimentações")    
+    else:   
+        print(extrato)
+    print(f"\nSaldo:\t\t R$ {saldo_atual:.2f}")
+    print("----------------------------------")
+
+def main():
+    menu = """
+-------------- MENU --------------
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -6,27 +25,31 @@ menu = """
 
 => """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
 
-while True:
-    opcao = input(menu)
+    while True:
+        opcao = input(menu)
 
-    if opcao == "d":
-        print("Depósito")
-        
-    elif opcao == "s":
-        print("Saque")
+        if opcao == "d":
+            valor = float(input("\nDigite o valor do depósito: "))
+            saldo, extrato = depositar(saldo, valor, extrato)
+            
+        elif opcao == "s":
+            print("Saque")
 
-    elif opcao == "e":
-        print("Extrato")
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato)
 
-    elif opcao == "q":
-        print("Obrigada por utilizar nosso sistema!")
-        break
+        elif opcao == "q":
+            print("Obrigada por utilizar nosso sistema!")
+            break
 
-    else:
-        print("Operação inválida. Por favor selecione novamente a operação desejada.")
+        else:
+            print("Operação inválida. Por favor selecione novamente a operação desejada.")
+
+
+main()
