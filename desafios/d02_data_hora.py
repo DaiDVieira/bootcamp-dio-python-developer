da73129acc2b6239e2a156cc022625fa807d0b11
DaiDVieira <daianevieira.ufsm@gmail.com>
2025-03-04 19:51:51 -0300

    criar e listar contas, data e hora no extrato

--- desafios/d02_data_hora.py
+++ desafios/d02_data_hora.py
@@ -1,22 +1,25 @@
-def depositar(saldo_atual, valor, extrato, /):
+from datetime import datetime
+
+def depositar(saldo_atual, valor, extrato, data, /):
     if valor > 0:
         saldo_atual += valor
-        extrato += f"Depósito:\t R$ {valor:.2f}\n"
+        extrato += f"Depósito:\t R$ {valor:.2f}\t {data}\n"
     else:
         print("Valor de depósito inválido. Tente novamente.")
+    print("------------------------------------------------")
 
     return saldo_atual, extrato
 
-def exibir_extrato(saldo_atual, extrato, /):
-    print("------------- Extrato ------------")
+def exibir_extrato(saldo_atual, extrato, data, /):
+    print("-------------------- Extrato -------------------")
     if not extrato:
         print("Não foram realizadas movimentações.")    
     else:   
         print(extrato)
-    print(f"\nSaldo:\t\t R$ {saldo_atual:.2f}")
-    print("----------------------------------")
+    print(f"\nSaldo:\t\t R$ {saldo_atual:.2f}\t {data}")
+    print("------------------------------------------------")
 
-def sacar(saldo_atual, valor, extrato, numero_saques, limite_valor, /):
+def sacar(saldo_atual, valor, extrato, numero_saques, limite_valor, data, /):
     if valor > 0:
         if saldo_atual < valor:
             print("Saldo insuficiente. Não será possível realizar o saque.")
@@ -26,10 +29,12 @@ def sacar(saldo_atual, valor, extrato, numero_saques, limite_valor, /):
             else:
                 saldo_atual -= valor
                 numero_saques += 1
-                extrato += f"Saque:\t\t R$ {valor:.2f}\n"
+                extrato += f"Saque:\t\t R$ {valor:.2f}\t {data}\n"
 
     else:
         print("Valor de saque inválido. Tente novamente.")
+
+    print("------------------------------------------------")
 
     return saldo_atual, extrato, numero_saques
 
@@ -47,48 +52,40 @@ def criar_usuario(usuarios, /):
 
     usuarios.append({"nome": nome, "cpf": cpf, "data_nascimento": data_nascimento, "endereco": endereco})
 
-    print("----- Usuário criado com sucesso! -----")
+    print("--------- Usuário criado com sucesso! ---------")
     
 
-def criar_conta(usuarios, contas, agencia, numero_conta, /):
+def criar_conta(usuarios, agencia, numero_conta, /):
     if usuarios != []:
         cpf = input("Digite o CPF do usuário: ")
         usuario = filtar_usuario(cpf, usuarios)
         print(f"teste usuario: {usuario}")
-        if usuario != None:
-            contas.append({"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario})
-            numero_conta += 1
+        if usuario:
+            print("--------- Conta criada com sucesso! ---------")
+            return ({"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario})
         else:
             print(f"Usuário de CPF {cpf} não está cadastrado. Operação cancelada.")
             return
     else:
         print("Não há usuários cadastrados. Cadastre um usuário antes de criar uma conta.")
 
+def filtar_usuario(cpf, usuarios, /):  
+    usuario_filtrado = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuario_filtrado[0] if usuario_filtrado else None
+
+def listar_contas(contas, /):
+    print("------------- Contas cadastradas -------------")
     for conta in contas:
-        print(conta)
-
-    print(numero_conta)
-
-    print("----- Conta criada com sucesso! -----")
-
-    return numero_conta
-
-def filtar_usuario(cpf, usuarios, /):
-    for usuario in usuarios:
-        if usuario["cpf"] == cpf:
-            posicao_usuario = usuarios.index(cpf)
-            return usuario[posicao_usuario]
-        else:
-            return None  
-    # usuario_filtrado = [usuarios.index(cpf) for usuario in usuarios if usuario["cpf"] == cpf]
-    # return usuario_filtrado[usuarios.index(cpf)] if usuario_filtrado else None
-
-def listar_contas(lista_contas):
-    pass
+        texto = f"""\
+Agência:\t\t{conta['agencia']}
+Conta Corrente:\t\t{conta['numero_conta']}
+Titular:\t\t{conta['usuario']['nome']}
+        """
+        print(texto)
 
 def main():
     menu = """
--------------- MENU --------------
+-------------------- MENU --------------------
 [1] Depositar
 [2] Sacar
 [3] Extrato
@@ -110,33 +107,42 @@ def main():
     numero_conta = 1
     AGENCIA = "0001"
 
-    print(usuarios)
-
     while True:
+        data = datetime.now()
+        data = data.strftime("%d/%m/%Y %H:%M:%S")
         opcao = input(menu)
+        data_hora_atual = datetime.now()
 
         if opcao == "1":
             valor = float(input("\nDigite o valor do depósito: "))
-            saldo, extrato = depositar(saldo, valor, extrato)
+            saldo, extrato = depositar(saldo, valor, extrato, data)  
+            print(f"{data_hora_atual} - DEPÓSITO")
             
         elif opcao == "2":
             if numero_saques == LIMITE_SAQUES:
                 print(f"Número máximo de {LIMITE_SAQUES} saques diários atingido. Tente novamente amanhã.")
             else:
                 valor = float(input("\nDigite o valor do saque: "))
-                saldo, extrato, numero_saques = sacar(saldo, valor, extrato, numero_saques, limite)
+                saldo, extrato, numero_saques = sacar(saldo, valor, extrato, numero_saques, limite, data)
+            print(f"{data_hora_atual} - SAQUE")
 
         elif opcao == "3":
-            exibir_extrato(saldo, extrato)
+            exibir_extrato(saldo, extrato, data)
+            print(f"{data_hora_atual} - EXTRATO")
 
         elif opcao == "4":
-            numero_conta = criar_conta(usuarios, contas, AGENCIA, numero_conta)
+            conta = criar_conta(usuarios, AGENCIA, numero_conta)
+            numero_conta += 1
+            contas.append(conta)
+            print(f"{data_hora_atual} - CRIAR_CONTA")
 
         elif opcao == "5":
-            print("Listar contas")
+            listar_contas(contas)
+            print(f"{data_hora_atual} - LISTAR_CONTAS")
 
         elif opcao == "6":
             criar_usuario(usuarios)
+            print(f"{data_hora_atual} - CRIAR_USUARIO")
 
         elif opcao == "0":
             print("Obrigada por utilizar nosso sistema!")
